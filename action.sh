da0292b010a089ca2712e0604d91c0fa6c2a0bd9
trudie69 <101123614+trudie69@users.noreply.github.com>
2022-03-14 19:21:24 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,6 @@
 branch=dev
-git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroidcp ultroid/.env /root/TeamUltroid/.envcd /root/TeamUltroiddocker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroiddocker run --privileged --env-file .env --rm -i ultroid
+git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
